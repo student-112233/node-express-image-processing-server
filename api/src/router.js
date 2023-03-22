3f400100f29771f29c46dee2d2d144e8e0737bc5
roviclester-enriquez <enriquez.rovic.lester.b.5151@gmail.com>
2023-03-22 11:48:00 +0800

    asd3
    asd3

--- api/src/router.js
+++ api/src/router.js
@@ -3,17 +3,17 @@ const multer = require('multer');
 const path = require('path');
 const imageProcessor = require('./imageProcessor');
 
+const photoPath = path.resolve(__dirname, '../../client/photo-viewer.html');
+
 const router = Router();
 
 const filename = (request, file, callback) => {
   callback(null, file.originalname);
 };
-
 const storage = multer.diskStorage({
   destination: 'api/uploads/',
   filename,
 });
-
 const fileFilter = (request, file, callback) => {
   if (file.mimetype !== 'image/png') {
     request.fileValidationError = 'Wrong file type';
@@ -22,16 +22,17 @@ const fileFilter = (request, file, callback) => {
     callback(null, true);
   }
 };
-
 const upload = multer({
   fileFilter,
   storage,
 });
-
 router.post('/upload', upload.single('photo'), (request, response) => {
   if (request.fileValidationError) return response.status(400).json({error: request.fileValidationError});
-
   return response.status(201).json({success: true});
+});
+
+router.get('/photo-viewer', (request, response) => {
+  response.sendFile(photoPath);
 });
 
 module.exports = router;
