80fcba4592bfca6e0fccd3671e50064cb3f1a21c
roviclester-enriquez <enriquez.rovic.lester.b.5151@gmail.com>
2023-03-22 11:50:40 +0800

    asd4
    asd4

--- api/src/router.js
+++ api/src/router.js
@@ -2,11 +2,8 @@ const {Router} = require('express');
 const multer = require('multer');
 const path = require('path');
 const imageProcessor = require('./imageProcessor');
-
 const photoPath = path.resolve(__dirname, '../../client/photo-viewer.html');
-
 const router = Router();
-
 const filename = (request, file, callback) => {
   callback(null, file.originalname);
 };
@@ -26,13 +23,20 @@ const upload = multer({
   fileFilter,
   storage,
 });
-router.post('/upload', upload.single('photo'), (request, response) => {
+
+router.post('/upload', upload.single('photo'), async (request, response) => {
   if (request.fileValidationError) return response.status(400).json({error: request.fileValidationError});
+
+  try {
+    await imageProcessor(request.file.filename);
+  } catch (error) {
+
+  }
+
   return response.status(201).json({success: true});
 });
 
 router.get('/photo-viewer', (request, response) => {
   response.sendFile(photoPath);
 });
-
 module.exports = router;
